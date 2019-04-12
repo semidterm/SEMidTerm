86bf2fe7a2c6272de71dd4e2e8de529299013bc6
DuyBuiTien <43126830+DuyBuiTien@users.noreply.github.com>
2019-04-12 17:25:18 +0700

    Get Post Method

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
 const express = require('express');
 const app = express();
-const port = 3000
+
+app.use(express.json());
+
+const port = process.env.port || 3000;
+
+const answers = [
+    { groupID : 1, chapterID: 1, result: 10},
+    { groupID : 2, chapterID: 1, result: 5},
+    { groupID : 3, chapterID: 1, result: 6},
+];
+
+
 
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '\\html\\MathFun.html');
+    res.sendFile(__dirname + '\\html\\MathFun.html');
 });
 
-app.post('/', function (req, res) {
-    res.send('Got a POST request')
+app.get('/:groupID', function (req, res) {
+    const answer = answers.find(c => c.groupID === parseInt(req.params.groupID))
+    if(!answer) {
+        res.status(404).send("The answer with the given groupID was not found")
+    }
+    res.send(answer);
 });
 
-app.put('/user', function (req, res) {
-    res.send('Got a PUT request at /user')
+app.get('/api', function(req,res) {
+    res.send(answers);
 })
 
-app.put('/user', function (req, res) {
-    res.send('Got a PUT request at /user')
-})
+app.post('/api', function (req, res) {
+    const answer = {
+        groupID   : 1,
+        chapterID : 2, 
+        result    : req.body.result
+    };
+    answers.push(answer);
+    res.status(200).send(answers);
+});
 
 app.use(express.static('html'))
 app.use(express.static('fonts'))
@@ -24,5 +45,3 @@ app.use(express.static('js'))
 app.use(express.static('owl-carousel'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
