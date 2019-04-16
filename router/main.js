48b1b5d9726dbfe9da82a6bd9a085aefab1af2b8
DuyBuiTien <43126830+DuyBuiTien@users.noreply.github.com>
2019-04-16 21:34:00 +0700

    Update main.js

--- router/main.js
+++ router/main.js
@@ -2,7 +2,11 @@ const path = require('path');
 
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, '..', 'views/MathFun.html'));
+        res.sendFile(path.join(__dirname, '..', 'MathFun.html'));
+    });
+
+    app.get('/Trangchu.html', (req, res) =>{
+        res.sendFile(path.join(__dirname,'..', 'Trangchu.html'))
     });
 
 
