6330aef833bb1c6a203db1b0306eba38815b83b1
DuyBuiTien <43126830+DuyBuiTien@users.noreply.github.com>
2019-04-17 16:30:39 +0700

    Ended fixing

--- router/main.js
+++ router/main.js
@@ -2,12 +2,25 @@ const path = require('path');
 
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, '..', 'MathFun.html'));
+        // res.sendFile(path.join(__dirname, '..', '/views/MathFun.html'));
+        res.sendFile(path.join(__dirname, '..', '/views/MathFun.html'));
     });
 
     app.get('/Trangchu.html', (req, res) =>{
-        res.sendFile(path.join(__dirname,'..', 'Trangchu.html'))
+        res.sendFile(path.join(__dirname, '..', '/views/Trangchu.html'))
     });
 
+    app.get('/Luyentap.html', (req, res) => {
+        res.sendFile(path.join(__dirname, '..', '/views/Luyentap.html'))
+    });
 
+    app.get('/Demden10.html', (req, res) => {
+        res.sendFile(path.join(__dirname, '..', '/views/Demden10.html'))
+    });
+
+    app.get('/Lienhe.html', (req, res) => {
+        res.sendFile(path.join(__dirname, '..', '/views/Lienhe.html'))
+    });
+
+    
 }
--- server.js
+++ server.js
@@ -3,13 +3,9 @@ const app = express();
 const path = require('path');
 
 const port = process.env.port || 3000;
-
+app.use( express.static(path.join(__dirname,'public')))
 require('./router/main')(app);
 app.set('views', path.join(__dirname, '/views'));
 // app.set('view engine', 'ejs');
-
-app.get('/')
-
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
