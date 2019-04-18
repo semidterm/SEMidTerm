1d15fc88831255feb37fe2ff73848e9b40c94be8
DuyBuiTien <43126830+DuyBuiTien@users.noreply.github.com>
2019-04-19 00:33:43 +0700

    update ajax

--- router/main.js
+++ router/main.js
@@ -2,7 +2,10 @@ const path = require('path');
 
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        // res.sendFile(path.join(__dirname, '..', '/views/MathFun.html'));
+        res.sendFile(path.join(__dirname, '..', '/views/MathFun.html'));
+    });
+
+    app.get('/MathFun.html', function (req, res) {
         res.sendFile(path.join(__dirname, '..', '/views/MathFun.html'));
     });
 
--- server.js
+++ server.js
@@ -4,8 +4,25 @@ const path = require('path');
 
 const port = process.env.port || 3000;
 app.use( express.static(path.join(__dirname,'public')))
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 require('./router/main')(app);
 app.set('views', path.join(__dirname, '/views'));
 // app.set('view engine', 'ejs');
+
+
+
+app.get('/demo',(req, res)=>{
+    let person = {
+        groupID: 1,
+        chapterID: 1,
+        question: "3 + 5 =",
+        result: 8
+    }
+    res.json(person);
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
